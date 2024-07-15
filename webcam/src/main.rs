5c0c352d43a043e14543224ebf6bfff77fde8262
Bey Hao Yun <beyhy94@gmail.com>
2024-07-15 21:59:05 +0800

    :hammer: Rectified main.rs in webcam to significantly reduce camera lag.
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

--- webcam/src/main.rs
+++ webcam/src/main.rs
@@ -1,43 +1,52 @@
-use anyhow::Result;
 use opencv::{
+    highgui,
     prelude::*,
     videoio,
-    highgui,
-    imgproc,
 };
+use std::time::Instant;
 
-fn main() -> Result<()> {
-    highgui::named_window("window", highgui::WINDOW_NORMAL)?;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize Camera
+    let setup_start = Instant::now();
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
-    let mut frame = Mat::default();
-    let mut resized_frame = Mat::default();
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
 
+    // If Camera is still unopened, exit program.
+    if !opened {
+        println!("Unable to open default camera!");
+        return Ok(());
+    }
+    // Set frame height and width
+    cam.set(videoio::CAP_PROP_FRAME_WIDTH, 640.0)?; 
+    cam.set(videoio::CAP_PROP_FRAME_HEIGHT, 480.0)?;
+    let setup_duration = setup_start.elapsed().as_secs_f64(); // Calculate setup duration
+    println!("Setup duration: {:?}", setup_duration); // Print setup duration
+
+    // Measure FPS
+    let fps_start = Instant::now();
+    let num_frames_to_measure = 60;
+    for _ in 0..num_frames_to_measure {
+        let mut temp_frame = Mat::default();
+        cam.read(&mut temp_frame)?;
+    }
+    let fps_duration = fps_start.elapsed();
+    let fps = num_frames_to_measure as f64 / fps_duration.as_secs_f64();
+    println!("Measured FPS: {:?}", fps);
+
+    // Display video stream from Camera
     loop {
+        let mut frame = Mat::default();
         cam.read(&mut frame)?;
-        
-        // Get the original frame dimensions
-        let original_height = frame.rows() as f64;
-        let original_width = frame.cols() as f64;
-        
-        // Calculate the new width while maintaining the aspect ratio
-        let new_height = 200.0;
-        let new_width = (original_width / original_height * new_height) as i32;
-        
-        // Resize the frame
-        imgproc::resize(
-            &frame,
-            &mut resized_frame,
-            opencv::core::Size::new(new_width, 200),
-            0.0,
-            0.0,
-            imgproc::INTER_LINEAR,
-        )?;
-
-        highgui::imshow("window", &resized_frame)?;
-        let key = highgui::wait_key(1)?;
-        if key == 113 { // quit with q
-            break;
+        if frame.size()?.width > 0 {
+            highgui::imshow("camera", &frame)?;
+            // Press 'q' program to exit
+            let key = highgui::wait_key(1)?;
+            if key == 13 {  
+                break;
+            }
         }
     }
+
     Ok(())
 }
